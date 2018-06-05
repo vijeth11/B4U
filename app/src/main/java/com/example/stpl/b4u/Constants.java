5f2ca4de84da0987b0eedcb151b12e250c929f80
vijethbelle <vijeth.belle@juspay.in>
2018-06-05 11:21:43 +0530

    added paytm payment gateway in staging mode

---
+++ app/src/main/java/com/example/stpl/b4u/Constants.java
@@ -0,0 +1,12 @@
+package com.example.stpl.b4u;
+
+public class Constants {
+
+    public static final String M_ID = "B4USta41878831649218"; //Paytm Merchand Id we got it in paytm credentials
+    public static final String CHANNEL_ID = "WAP"; //Paytm Channel Id, got it in paytm credentials
+    public static final String INDUSTRY_TYPE_ID = "Retail"; //Paytm industry type got it in paytm credential
+
+    public static final String WEBSITE = "APPSTAGING";
+    public static final String CALLBACK_URL = "https://securegw.paytm.in/theia/paytmCallback?ORDER_ID=";
+
+}
--- app/src/main/java/com/example/stpl/b4u/MainActivity.java
+++ app/src/main/java/com/example/stpl/b4u/MainActivity.java
@@ -4,12 +4,16 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.BottomSheetBehavior;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -35,6 +39,15 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.WrapperListAdapter;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.paytm.pgsdk.PaytmOrder;
+import com.paytm.pgsdk.PaytmPGService;
+import com.paytm.pgsdk.PaytmPaymentTransactionCallback;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -43,10 +56,17 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.zip.GZIPOutputStream;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener,PaytmPaymentTransactionCallback {
 
     private BottomSheetBehavior sheetBehavior;
     public static Integer ItemSelected=0;
@@ -168,10 +188,12 @@ public class MainActivity extends AppCompatActivity
                     RadioButton inB4u=(RadioButton)view.findViewById(R.id.isB4u);
                     if(parcel.isChecked()) {
                         // Toast.makeText(MainActivity.this, "No poytm cash bro plox ;_;", Toast.LENGTH_SHORT).show();
+                        generateCheckSum();
                         Intent menuIntent = new Intent(MainActivity.this, Main2Activity.class).putExtra("orders","Parcel:\n"+orderlist.getText().toString()).putExtra("totalCost",totalCost.getText().toString());
                         startActivity(menuIntent);
                     }
                     else if(inB4u.isChecked()) {
+                        generateCheckSum();
                         Intent menuIntent = new Intent(MainActivity.this, Main2Activity.class).putExtra("orders",orderlist.getText().toString()).putExtra("totalCost",totalCost.getText().toString());
                         startActivity(menuIntent);
                         //Toast.makeText(MainActivity.this, "No cash bro plox ;_;", Toast.LENGTH_SHORT).show();
@@ -186,7 +208,138 @@ public class MainActivity extends AppCompatActivity
         dialog=builder.create();
         dialog.show();
     }
+    private void generateCheckSum() {
 
+        //getting the tax amount first.
+        String txnAmount = totalCost.getText().toString().trim();
+        txnAmount=String.valueOf((int)Float.parseFloat(txnAmount.replace("₹","")));
+        //creating a retrofit object.
+        txnAmount+=".00";
+
+        Toast.makeText(MainActivity.this,"amount at generateChecksum="+txnAmount,Toast.LENGTH_SHORT).show();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(Api.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        //creating the retrofit api service
+        Api apiService = retrofit.create(Api.class);
+
+        //creating paytm object
+        //containing all the values required
+        final Paytm paytm = new Paytm(
+                Constants.M_ID,
+                Constants.CHANNEL_ID,
+                txnAmount,
+                Constants.WEBSITE,
+                Constants.CALLBACK_URL,
+                Constants.INDUSTRY_TYPE_ID
+        );
+
+        //creating a call object from the apiService
+        Call<Checksum> call = apiService.getChecksum(
+                paytm.getmId(),
+                paytm.getOrderId(),
+                paytm.getCustId(),
+                paytm.getChannelId(),
+                paytm.getTxnAmount(),
+                paytm.getWebsite(),
+                paytm.getCallBackUrl(),
+                paytm.getIndustryTypeId()
+        );
+
+        //making the call to generate checksum
+        call.enqueue(new Callback<Checksum>() {
+            @Override
+            public void onResponse(Call<Checksum> call, Response<Checksum> response) {
+
+                //once we get the checksum we will initiailize the payment.
+                //the method is taking the checksum we got and the paytm object as the parameter
+                Toast.makeText(MainActivity.this,"amount at initpayment" + "="+paytm.getTxnAmount(),Toast.LENGTH_SHORT).show();
+                initializePaytmPayment(response.body().getChecksumHash(), paytm);
+
+            }
+
+            @Override
+            public void onFailure(Call<Checksum> call, Throwable t) {
+
+            }
+        });
+
+
+   }
+
+
+    private void initializePaytmPayment(String checksumHash, Paytm paytm) {
+
+        //getting paytm service
+        PaytmPGService Service = PaytmPGService.getStagingService();
+
+        //use this when using for production
+        //PaytmPGService Service = PaytmPGService.getProductionService();
+
+
+        //creating a hashmap and adding all the values required
+        Map<String, String> paramMap = new HashMap<>();
+        paramMap.put("MID", Constants.M_ID);
+        paramMap.put("ORDER_ID", paytm.getOrderId());
+        paramMap.put("CUST_ID", paytm.getCustId());
+        paramMap.put("CHANNEL_ID", paytm.getChannelId());
+        paramMap.put("TXN_AMOUNT", paytm.getTxnAmount());
+        paramMap.put("WEBSITE", paytm.getWebsite());
+        paramMap.put("CALLBACK_URL", paytm.getCallBackUrl());
+        paramMap.put("CHECKSUMHASH", checksumHash);
+        paramMap.put("INDUSTRY_TYPE_ID", paytm.getIndustryTypeId());
+
+
+        //creating a paytm order object using the hashmap
+        PaytmOrder order = new PaytmOrder(paramMap);
+
+
+        //intializing the paytm service
+        Service.initialize(order, null);
+
+        //finally starting the payment transaction
+        Service.startPaymentTransaction(this, true, true, this);
+
+    }
+
+    //all these overriden method is to detect the payment result accordingly
+    @Override
+    public void onTransactionResponse(Bundle bundle) {
+
+        Toast.makeText(this, bundle.toString(), Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void networkNotAvailable() {
+        Toast.makeText(this, "Network error", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void clientAuthenticationFailed(String s) {
+        Toast.makeText(this, s, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void someUIErrorOccurred(String s) {
+        Toast.makeText(this, s, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onErrorLoadingWebPage(int i, String s, String s1) {
+        Toast.makeText(this, s, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onBackPressedCancelTransaction() {
+        Toast.makeText(this, "Back Pressed", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onTransactionCancel(String s, Bundle bundle) {
+        Toast.makeText(this, s + bundle.toString(), Toast.LENGTH_LONG).show();
+    }
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
